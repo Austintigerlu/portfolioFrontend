4b0e258c95f0ace3ad092c1b88a393d2679f055b
austintigerlu <austintigerlu@gmail.com>
2022-05-11 12:43:49 -0400

    fixed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,32 +1,39 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-function Header(props) {
-  //inline style for the nav tag
-  const navStyle = {
-    display: "flex",
-    justifyContent: "space-around",
-    border: "3px solid black",
-    padding: "8px",
-    width: "90%",
-    margin: "auto",
+function Projects(props) {
+  // create state to hold projects
+  const [projects, setProjects] = useState(null);
+
+  //create function to make api call
+  const getProjectsData = async () => {
+    //make api call and get response
+    const response = await fetch(props.URL + "projects");
+    // turn response into javascript object
+    const data = await response.json();
+    // set the projects state to the data
+    setProjects(data);
   };
 
-  return (
-    <header>
-      <h1>My Portfolio Page</h1>
-      <nav style={navStyle}>
-        <Link to="/">
-          <div>HOME</div>
-        </Link>
-        <Link to="/about">
-          <div>ABOUT</div>
-        </Link>
-        <Link to="/projects">
-          <div>PROJECTS</div>
-        </Link>
-      </nav>
-    </header>
-  );
+  // make an initial call for the data inside a useEffect, so it only happens once on component load
+  useEffect(() => getProjectsData(), []);
+
+  // define a function that will return the JSX needed once we get the data
+  const loaded = () => {
+    return projects.map((project) => (
+      <div>
+        <h1>{project.name}</h1>
+        <img src={project.image} />
+        <a href={project.git}>
+          <button>Github</button>
+        </a>
+        <a href={project.live}>
+          <button>live site</button>
+        </a>
+      </div>
+    ));
+  };
+
+  return projects ? loaded() : <h1>Loading...</h1>;
 }
 
-export default Header;
+export default Projects;
