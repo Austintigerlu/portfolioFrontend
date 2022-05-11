b66deabc44d6ba0e9ef1fd4a2dd743750610bed2
austintigerlu <austintigerlu@gmail.com>
2022-05-11 12:35:30 -0400

    frontend

---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+function About(props) {
+    const [about, setAbout] = useState("");
+
+    const getAbout = async () => {
+        const response = await fetch(props.URL + "about");
+        const data = await response.json();
+        setAbout(data)
+    };
+
+    useEffect(()=> getAbout(), []);
+
+    const loaded = () => (
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    );
+    return about ? loaded() : <h1>Loading...</h1>
+  }
+  
+  export default About;
